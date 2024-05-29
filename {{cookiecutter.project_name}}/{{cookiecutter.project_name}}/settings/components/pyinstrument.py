633bcd082133c2c15be2a1bc73b7ca9afa5c6a05
Waseem Akram <waseem07799@gmail.com>
2024-05-29 23:25:31 +0530

    feat: remove pyinstrument callback

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings/components/pyinstrument.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings/components/pyinstrument.py
@@ -1,11 +1,5 @@
-from django.http import HttpRequest
-from optik_invoice.settings.components.common import MIDDLEWARE
+# https://pypi.org/project/pyinstrument/3.0.0b3/#profile-a-web-request-in-django
+
+from {{cookiecutter.project_name}}.settings.components.common import MIDDLEWARE
 
 MIDDLEWARE += ["pyinstrument.middleware.ProfilerMiddleware"]
-
-
-def custom_show_pyinstrument(request: HttpRequest) -> bool:
-    return request.user.is_superuser
-
-
-PYINSTRUMENT_SHOW_CALLBACK = f"{__name__}.custom_show_pyinstrument"
