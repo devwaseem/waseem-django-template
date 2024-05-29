998fc3fe526a10097a24e3de75a367e9cf47b5ef
Waseem Akram <waseem07799@gmail.com>
2024-05-29 23:39:55 +0530

    fix: support csp for pyinstrument

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings/components/pyinstrument.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings/components/pyinstrument.py
@@ -1,5 +1,10 @@
 # https://pypi.org/project/pyinstrument/3.0.0b3/#profile-a-web-request-in-django
 
 from {{cookiecutter.project_name}}.settings.components.common import MIDDLEWARE
+from {{cookiecutter.project_name}}.settings.components.csp import CSP_SCRIPT_SRC
 
 MIDDLEWARE += ["pyinstrument.middleware.ProfilerMiddleware"]
+
+# Temporary workaround until Pyinstrument supports CSP
+CSP_SCRIPT_SRC += ("'unsafe-inline'",)
+
