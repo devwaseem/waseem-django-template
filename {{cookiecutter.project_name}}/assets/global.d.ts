41d3f07fad74817f5f1ef2ae2453e86d45d82d36
Waseem Akram <waseem07799@gmail.com>
2024-06-10 18:27:53 +0530

    fix: typescript errors

--- {{cookiecutter.project_name}}/assets/global.d.ts
+++ {{cookiecutter.project_name}}/assets/global.d.ts
@@ -4,6 +4,7 @@ declare global {
     interface Window {
         Alpine: AlpineType;
         $byId: Function;
+        openLinkInNewWindow: (string) => void;
     }
     const Alpine: AlpineType;
 }
