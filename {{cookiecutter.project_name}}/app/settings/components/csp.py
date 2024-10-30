e47f527101f8c721d307fb6ecef07731b8b6b716
Waseem Akram <waseem07799@gmail.com>
2024-10-30 17:26:23 +0530

    feat: update csp settings

--- {{cookiecutter.project_name}}/app/settings/components/csp.py
+++ {{cookiecutter.project_name}}/app/settings/components/csp.py
@@ -14,17 +14,16 @@ CSP_INCLUDE_NONCE_IN = ("script-src", "connect-src")
 CSP_EXCLUDE_URL_PREFIXES = ("/admin",)
 CSP_SCRIPT_SRC: tuple[str, ...] = (
     "'self'",
+    "blob:",
     "'unsafe-eval'",
     "https://unpkg.com",
-    STATIC_URL,
 )
+
 CSP_IMG_SRC: tuple[str, ...] = (
     "'self'",
     "data:",  # Required by tailwind
     "http:",
     "https:",
-    STATIC_URL,
-    MEDIA_URL,
 )
 CSP_FONT_SRC: tuple[str, ...] = (
     "'self'",
@@ -32,24 +31,29 @@ CSP_FONT_SRC: tuple[str, ...] = (
     "https://fonts.gstatic.com",
     "data:",
     "'unsafe-inline'",
-    STATIC_URL,
 )
 CSP_STYLE_SRC: tuple[str, ...] = (
     "'self'",
     "https://fonts.googleapis.com",
     "https://unpkg.com",
     "'unsafe-inline'",
-    STATIC_URL,
 )
 CSP_MEDIA_SRC: tuple[str, ...] = (
     "'self'",
     "data:",
-    STATIC_URL,
-    MEDIA_URL,
 )
 CSP_DEFAULT_SRC: tuple[str, ...] = ("'self'",)
-CSP_CONNECT_SRC: tuple[str, ...] = (
-    "'self'",
-    STATIC_URL,
-    MEDIA_URL,
-)
+CSP_CONNECT_SRC: tuple[str, ...] = ("'self'",)
+
+if STATIC_URL.startswith("http"):
+    CSP_SCRIPT_SRC += (STATIC_URL,)
+    CSP_IMG_SRC += (STATIC_URL,)
+    CSP_FONT_SRC += (STATIC_URL,)
+    CSP_STYLE_SRC += (STATIC_URL,)
+    CSP_MEDIA_SRC += (STATIC_URL,)
+    CSP_CONNECT_SRC += (STATIC_URL,)
+
+if MEDIA_URL.startswith("http"):
+    CSP_IMG_SRC += (MEDIA_URL,)
+    CSP_MEDIA_SRC += (MEDIA_URL,)
+    CSP_CONNECT_SRC += (MEDIA_URL,)
