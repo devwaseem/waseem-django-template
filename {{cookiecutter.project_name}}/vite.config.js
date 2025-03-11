64d13f4acf1f0b3159e9a69f97e3e23f6c6b95bb
Waseem Akram <waseem07799@gmail.com>
2025-03-11 16:27:16 +0530

    feat: use vite plugin for django frontend kit

--- {{cookiecutter.project_name}}/vite.config.js
+++ {{cookiecutter.project_name}}/vite.config.js
@@ -1,31 +1,7 @@
-import { glob } from "glob";
 import path from "path";
 import { defineConfig, loadEnv } from "vite";
 import tailwindcss from '@tailwindcss/vite'
-
-async function buildRollupInput(frontendDirs) {
-  let rollupInput = {};
-  for (let dir of frontendDirs) {
-    dir = path.resolve(dir);
-    for (let subDir of ["pages", "shared", "layout"]) {
-      let subDirPath = dir + "/" + subDir;
-      let resolved_files = await glob(subDirPath + "/**/*.{js,ts}");
-      for (let file of resolved_files) {
-        let key = path.resolve(file).replace(dir, "");
-        if (key.startsWith("/")) {
-          key = key.slice(1);
-        }
-        key = key.replace(/(index)?\.(js|ts|css)$/g, "");
-        key = key.replace(/\//g, "-");
-        if (key.endsWith("-")) {
-          key = key.slice(0, -1);
-        }
-        rollupInput[key] = file;
-      }
-    }
-  }
-  return rollupInput;
-}
+import VitePluginDjangoFrontendKit from "@iamwaseem99/vite-plugin-django-frontend-kit";
 
 export default defineConfig(async ({ mode }) => {
   const env = loadEnv(mode, process.cwd());
@@ -35,6 +11,10 @@ export default defineConfig(async ({ mode }) => {
     root: ".",
     plugins: [
       tailwindcss(),
+      VitePluginDjangoFrontendKit({
+        frontendDirs: ['./frontend'],
+        glob_pattern: "/**/*.{js,ts}"
+      })
     ],
     resolve: {
       alias: {
@@ -51,9 +31,6 @@ export default defineConfig(async ({ mode }) => {
       emptyOutDir: true,
       sourcemap: isDevelopment ? "inline" : false,
       minify: isDevelopment ? false : "esbuild",
-      rollupOptions: {
-        input: await buildRollupInput(["./frontend"]),
-      },
     },
   };
 });
